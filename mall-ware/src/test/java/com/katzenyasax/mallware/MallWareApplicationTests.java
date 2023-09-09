a6c953ca55264b7c95cfa327db63f8f6049ee36b
InnerSekiro <a18290531268@163.com>
2023-09-09 08:33:07 +0800

    基础框架搭建完成

---
+++ mall-ware/src/test/java/com/katzenyasax/mallware/MallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.katzenyasax.mallware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
